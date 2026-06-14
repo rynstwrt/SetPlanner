de79ec876b437a10607792cc89398fa223d23cf8
Ryn. <rynstwrt17@gmail.com>
2026-06-14 03:09:07 -0500

    Convert to song class

--- src/Song.js
+++ src/Song.js
@@ -1,7 +1,165 @@
+const fs = require("fs");
+const {parseStream} = require("music-metadata");
+
+
+
+/*
+ * PERFECT MATCH:
+ *   - same key
+ *   - 2A->1B (-1 and change letter if A)
+ *   - 1B->2A (+1 and change letter if B)
+ *
+ *
+ *
+ * ENERGY+:
+ *   - 2A->2B (change letter if A)
+ *   - 2A->3A (+1)
+ *
+ * ENERGY++:
+ *   - 2A->11A (-3)
+ *
+ * ENERGY+++:
+ *   - 2A->4A (+2)
+ *   - 2A->9A (-5)
+ *
+ *
+ *
+ * ENERGY-:
+ *   - 2A->1A (-1)
+ *   - 2B->2A (change letter if B)
+ *
+ * ENERGY--:
+ *   - 2A->5A (+3)
+ *
+ * ENERGY---:
+ *   - 2A->12A (-2)
+ *   - 2A->7A (+5)
+ */
+
+
+
+const shiftCamelotNum = (camelotNum, shift) => ((parseInt(camelotNum) + shift + 12) % 12) || 12;
+const flipCamelotLetter = (camelotLetter) => camelotLetter === "A" ? "B" : "A";
+
+
+function changeCamelot(key, numShift = 0, changeLetter = false) {
+    const matches = key.match(/(\d{1,2})([A|B])/);
+    const [_, camelotNum, camelotLetter] = matches;
+
+    const shiftedNum = shiftCamelotNum(camelotNum, numShift);
+    const flippedOrUnflippedLetter = changeLetter ? flipCamelotLetter(camelotLetter) : camelotLetter;
+    return `${shiftedNum}${flippedOrUnflippedLetter}`;
+}
 
 
 class Song {
-    constructor(filePath, key) {
+    constructor(filePath) {
+        this.path = filePath;
+        this.key = undefined;
 
+        this.camelotNum = undefined;
+        this.camelotLetter = undefined;
+    }
+
+
+    async loadKey() {
+        const audioStream = fs.createReadStream(this.path);
+        const metadata = await parseStream(audioStream, {mimeType: "audio/mpeg"});
+
+        this.key = metadata.common.key;
+        if (!this.key) {
+            console.warn(`File ${this.path} does not have a key!`);
+            return false;
+        }
+
+        const matches = this.key.match(/(\d{1,2})([A|B])/);
+        this.camelotNum = matches[1];
+        this.camelotLetter = matches[2];
+        return true;
+    }
+
+
+    /*
+      EXACT MATCH:
+        - Same key
+        - 2A->1B (-1 and change letter if A)
+        - 1B->2A (+1 and change letter if B)
+     */
+    getExactMatchingKeys() {
+        return [this.key, changeCamelot(this.key, (this.camelotLetter === "A" ? -1 : 1), true)];
+    }
+
+
+    /*
+      ENERGY+:
+        - 2A->2B (change letter if A)
+        - 2A->3A (+1)
+    */
+    getEnergyBoost1Keys() {
+        const energyBoost1Keys = [];
+
+        if (this.camelotLetter === "A")
+            energyBoost1Keys.push(changeCamelot(this.key, 0, true));
+
+        energyBoost1Keys.push(changeCamelot(this.key, 1));
+
+        return energyBoost1Keys;
+    }
+
+
+    /*
+      ENERGY++:
+        - 2A->11A (-3)
+     */
+    getEnergyBoost2Keys() {
+        return [changeCamelot(this.key, -3)];
+    }
+
+
+    /*
+      ENERGY+++:
+        - 2A->4A (+2)
+        - 2A->9A (-5)
+     */
+    getEnergyBoost3Keys() {
+        return [changeCamelot(this.key, 2), changeCamelot(this.key, -5)];
+    }
+
+
+
+    /*
+      ENERGY-:
+        - 2A->1A (-1)
+        - 2B->2A (change letter if B)
+     */
+    getEnergyDrop1Keys() {
+        const energyDrop1Keys = [changeCamelot(this.key, -1)];
+
+        if (this.camelotLetter === "B")
+            energyDrop1Keys.push(changeCamelot(this.key, 0, true));
+
+        return energyDrop1Keys;
+    }
+
+
+    /*
+      ENERGY--:
+        - 2A->5A (+3)
+     */
+    getEnergyDrop2Keys() {
+        return [changeCamelot(this.key, 3)];
+    }
+
+
+    /*
+      ENERGY---:
+        - 2A->12A (-2)
+        - 2A->7A (+5)
+     */
+    getEnergyDrop3Keys() {
+        return [changeCamelot(this.key, -2), changeCamelot(this.key, 5)];
     }
 }
+
+
+module.exports = {Song};
--- src/index.js
+++ src/index.js
@@ -1,39 +1,5 @@
-const fs = require("fs");
-const path = require("path");
-const {parseStream} = require("music-metadata");
-
-
-const MUSIC_DIR = "test/music";
-// const START_FILE = path.join(MUSIC_DIR, "Amy Kisnorbo - Squeeeze");
-
-
-let setList = [];
-
-
-function getFilesWithKey() {
-    return new Promise((res, rej) => {
-        fs.readdir(MUSIC_DIR, (err, files) => {
-            const audioFiles = [];
-
-            files.forEach(async (file, i) => {
-                const filePath = path.join(MUSIC_DIR, file)
-
-                const audioStream = fs.createReadStream(filePath);
-                const metadata = await parseStream(audioStream, {mimeType: "audio/mpeg"});
-
-                const key = metadata.common.key;
-                if (!key)
-                    return console.warn(`File ${filePath} does not have a key!`);
-
-                audioFiles.push({path: filePath, key: key});
-
-                if (i === files.length - 1)
-                    res(audioFiles)
-            });
-        });
-
-    });
-}
+const {globby} = require("globby");
+const {Song} = require("./Song.js");
 
 
 function chooseRandom(list) {
@@ -42,116 +8,45 @@ function chooseRandom(list) {
 
 
 
-/*  
- * PERFECT MATCH:
- *   - same key 
- *   - 2A->1B (-1 and change letter if A)
- *   - 1B->2A (+1 and change letter if B)
- * 
- * 
- * 
- * ENERGY+:
- *   - 2A->2B (change letter)
- *   - 2A->3A (+1)
- * 
- * ENERGY++:
- *   - 2A->11A (-3)
- * 
- * ENERGY+++:
- *   - 2A->4A (+2)
- *   - 2A->9A (-5)
- * 
- * 
- * 
- * ENERGY-:
- *   - 2A->1A (-1)
- *  
- * ENERGY--: 
- *   - 2A->5A (+3)
- * 
- * ENERGY---:
- *   - 2A->12A (-2)
- *   - 2A->7A (+5)
- */
-let unusedSongs;
+function selectNextSong(prevSong) {
+    console.log(`Finding unusedSongs compatible with key: ${prevSong.key}`)
 
 
-// function shiftCamelotNum(camelotNum, shift) {
-//     const rolled = (camelotNum + shift + 12) % 12;
-//     return rolled || 12;
-//     // return rolled === 0 ? 12 : rolled;
-// }
-
-
-const shiftCamelotNum = (camelotNum, shift) => ((parseInt(camelotNum) + shift + 12) % 12) || 12;
-const flipCamelotLetter = (camelotLetter) => camelotLetter === "A" ? "B" : "A";
-
-
-function changeCamelot(key, numShift=0, changeLetter=false) {
-    const matches = key.match(/(\d{1,2})([A|B])/);
-    const [_, camelotNum, camelotLetter] = matches;
-
-    const shiftedNum = shiftCamelotNum(camelotNum, numShift);
-    const flippedOrUnflippedLetter = changeLetter ? flipCamelotLetter(camelotLetter) : camelotLetter;
-    return `${shiftedNum}${flippedOrUnflippedLetter}`;
-}
-
-
-function selectNextSong(key) {
-    const matches = key.match(/(\d{1,2})([A|B])/);
-    const [_, camelotNum, camelotLetter] = matches;
-
-    console.log(`Finding unusedSongs compatible with key: ${key}`)
-
-    // Find exact song key matches
-    const exactMatches = unusedSongs.filter(song => {
-        if (song.key === key)
-            return true;
-
-        if (camelotLetter === "A" && song.key === changeCamelot(key, -1, true))
-            return true;
-
-        if (camelotLetter === "B" && song.key === changeCamelot(key, 1, true))
-            return true;
-        // let targetCamelotLetter = (camelotLetter === "A") ? "B" : "A";
-        // let targetCamelotNum = (targetCamelotLetter === "A") ? camelotNum-1 : camelotNum+1;
-
-        return false;
-    });
-
-    console.log(`exact matches:`);
-    exactMatches.forEach(exactMatch => console.log(exactMatch));
-
+    const exactMatches = unusedSongs.filter(song => prevSong.getExactMatchingKeys().includes(song.key));
     if (exactMatches.length)
         // return exact Matches[Math.floor(Math.random() * exactMatches.length)];
         return chooseRandom(exactMatches);
 
-    // TODO: Find energy boost matches
-    const energyBoostMatches = [];
-    if (energyBoostMatches.length) 
-        return chooseRandom(energyBoostMatches);
-    
-    const energyBoost2Matches = [];
-    if (energyBoost2Matches.length) 
+
+
+    // Find energy boost matches
+    const energyBoost1Matches = unusedSongs.filter(song => prevSong.getEnergyBoost1Keys().includes(song.key));
+    if (energyBoost1Matches.length)
+        return chooseRandom(energyBoost1Matches);
+
+    const energyBoost2Matches = unusedSongs.filter(song => prevSong.getEnergyBoost2Keys().includes(song.key));
+    if (energyBoost2Matches.length)
         return chooseRandom(energyBoost2Matches);
-    
-    const energyBoost3Matches = [];
-    if (energyBoost3Matches.length) 
+
+    const energyBoost3Matches = unusedSongs.filter(song => prevSong.getEnergyBoost3Keys().includes(song.key));
+    if (energyBoost3Matches.length)
         return chooseRandom(energyBoost3Matches);
 
 
-    // TODO: Find energy Drop matches
-    const energyDropMatches = [];
-    if (energyDropMatches.length)
-        return chooseRandom(energyDropMatches);
 
-    const energyDrop2Matches = [];
+    // Find energy Drop matches
+    const energyDrop1Matches = unusedSongs.filter(song => prevSong.getEnergyDrop1Keys().includes(song.key));
+    if (energyDrop1Matches.length)
+        return chooseRandom(energyDrop1Matches);
+
+    const energyDrop2Matches = unusedSongs.filter(song => prevSong.getEnergyDrop2Keys().includes(song.key));
     if (energyDrop2Matches.length)
         return chooseRandom(energyDrop2Matches);
 
-    const energyDrop3Matches = [];
+    const energyDrop3Matches = unusedSongs.filter(song => prevSong.getEnergyDrop3Keys().includes(song.key));
     if (energyDrop3Matches.length)
         return chooseRandom(energyDrop3Matches);
+
 
 
     // Randomly select if nothing else found
@@ -159,6 +54,8 @@ function selectNextSong(key) {
 }
 
 
+
+let setList = [];
 function addSongToSet(song) {
     setList.push(song);
     unusedSongs.splice(unusedSongs.indexOf(song), 1);
@@ -166,25 +63,24 @@ function addSongToSet(song) {
 
 
 
+const unusedSongs = [];
 (async () => {
-    unusedSongs = await getFilesWithKey();
+    const songPaths = await globby("./test/music/**/*");
 
-    // const startSongIdx = Math.floor(Math.random() * unusedSongs.length);
-    // const startSong = unusedSongs[startSongIdx];
-    // unusedSongs.splice(startSongIdx, 1);
-    // setList.push(startSong);
+    for (const songPath of songPaths) {
+        const song = new Song(songPath);
+        if (await song.loadKey())
+            unusedSongs.push(song);
+    }
+
     const startSong = chooseRandom(unusedSongs);
     addSongToSet(startSong);
 
     for (let i = 0; i < 10; ++i) {
-        const lastUsedSong = setList[setList.length - 1];
-        const nextSong = selectNextSong(lastUsedSong.key);
-
+        const nextSong = selectNextSong(setList[setList.length-1]);
         addSongToSet(nextSong);
-        // const nextSongIdx = unusedSongs.indexOf(nextSong);
-        // unusedSongs.splice(nextSongIdx, 1);
-        // setList.push(nextSong);
     }
 
-    // console.log(setList) 
+    console.log("FINISHED SETLIST:");
+    console.log(setList);
 })();
