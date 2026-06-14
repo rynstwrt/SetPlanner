063fa7364df5cdeee939238da97e741e57dd762a
Ryn. <rynstwrt17@gmail.com>
2026-06-14 01:36:16 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const {parseStream} = require("music-metadata");
 
 
 const MUSIC_DIR = "test/music";
-const START_FILE = path.join(MUSIC_DIR, "Amy Kisnorbo - Squeeeze");
+// const START_FILE = path.join(MUSIC_DIR, "Amy Kisnorbo - Squeeeze");
 
 
 let usedSongs = [];
@@ -36,13 +36,101 @@ function getFilesWithKey() {
 }
 
 
-function findUnusedCompatibleSongsInKey(key) {
+/*  
+ * PERFECT MATCH:
+ *   - same key 
+ *   - 2A->1B (-1 and change letter if A)
+ *   - 1B->2A (+1 and change letter if B)
+ * 
+ * 
+ * 
+ * ENERGY+:
+ *   - 2A->2B (change letter)
+ *   - 2A->3A (+1)
+ * 
+ * ENERGY++:
+ *   - 2A->11A (-3)
+ * 
+ * ENERGY+++:
+ *   - 2A->4A (+2)
+ *   - 2A->9A (-5)
+ * 
+ * 
+ * 
+ * ENERGY-:
+ *   - 2A->1A (-1)
+ *  
+ * ENERGY--: 
+ *   - 2A->5A (+3)
+ * 
+ * ENERGY---:
+ *   - 2A->12A (-2)
+ *   - 2A->7A (+5)
+ */
+let songs;
 
+
+// function shiftCamelotNum(camelotNum, shift) {
+//     const rolled = (camelotNum + shift + 12) % 12;
+//     return rolled || 12;
+//     // return rolled === 0 ? 12 : rolled;
+// }
+
+
+const shiftCamelotNum = (camelotNum, shift) => (camelotNum + shift + 12) % 12 || 12;
+
+
+function findUnusedCompatibleSongsInKey(key) {
+    const matches = key.match(/(\d{1,2})([A|B])/);
+    const [_, camelotNum, camelotLetter] = matches;
+
+    console.log(camelotNum, camelotLetter)
+
+    // Find exact song key matches
+    return songs.filter(song => {
+        if (song.key === key)
+            return true;
+
+        let targetCamelotLetter = (camelotLetter === "A") ? "B" : "A";
+        let targetCamelotNum = (targetCamelotLetter === "A") ? camelotNum-1 : camelotNum+1;
+
+    });
+
+    // TODO: Find energy boost matches
+
+    // TODO: Find energy drop matches
+
+    // TODO: Randomly select if nothing else found
+    
+    // for (const match of matches) {
+    //     console.log(match)
+    // }
+    // songs.forEach(song => {
+
+    // });
 }
 
 
-(async () => {
-    const files = await getFilesWithKey();
 
-    
+(async () => {
+    // console.log(13 % 12);
+    // console.log((1 - 1 + 12) % 12);
+    console.log(shiftCamelotNum(1, 1));
+    console.log(shiftCamelotNum(12, 1));
+    console.log(shiftCamelotNum(1, -1));
+    return;
+    songs = await getFilesWithKey();
+
+    const startSongIdx = Math.floor(Math.random() * songs.length);
+    const startSong = songs[startSongIdx];
+    songs.splice(startSongIdx, 1);
+
+    usedSongs.push(startSong);
+
+    for (let i = 0; i < 10; ++i) {
+        const lastUsedSong = usedSongs[usedSongs.length - 1];
+        const nextSong = findUnusedCompatibleSongsInKey(lastUsedSong.key);
+        // songs.splice(startSongIdx, 1);
+        // usedSongs.push(nextSong);
+    }
 })();
